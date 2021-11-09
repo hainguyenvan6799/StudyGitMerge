2944fd024ebe228422a1fa642195a76d2eb2f636
Hai Nguyen Van <hai-nguyen@cybozu.vn>
2021-11-09 11:02:50 +0700

    do something after see conflict in remote but do not resolve conflict.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function doKCB1472() {
     console.log("do something KCB-1472");
     console.log("do something after reviewed code by mentor");
+    console.log("do something after see conflict in remote but do not resolve conflict.");
 }
